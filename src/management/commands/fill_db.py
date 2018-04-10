c91333c997b25027062c80574ace1d68271e3ed9
ksanvat <ksanworks@gmail.com>
2018-04-10 07:06:01 +0300

    upd

--- src/management/commands/fill_db.py
+++ src/management/commands/fill_db.py
@@ -1,3 +1,5 @@
+import os
+
 from django.core.management.base import BaseCommand
 
 from src.db.works.parser.one import shop_01_users
@@ -5,6 +7,7 @@ from src.db.works.parser.one import shop_01_speed
 from src.db.works.parser.one import shop_01_demand
 
 from src.db.works.parser.two import shop_003
+from src.db.works.parser.three import shop_004
 
 
 class Command(BaseCommand):
@@ -14,7 +17,10 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
+        os.system('./manage.py flush --noinput')
+        print('Old database flushed')
         # shop_01_users.run()
         # shop_01_speed.run()
         # shop_01_demand.run()
-        shop_003.run()
+        # shop_003.run()
+        shop_004.run()
