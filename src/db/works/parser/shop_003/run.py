2910cb4117a43932d5f2483feb20ac8b3c48b989
ksanvat <ksanworks@gmail.com>
2018-04-20 10:36:01 +0300

    upd

--- src/db/works/parser/shop_003/run.py
+++ src/db/works/parser/shop_003/run.py
@@ -1,7 +1,7 @@
 import os
 
 from src.db.models import SuperShop
-from . import users, users_2, demand
+from . import users, users_2, demand, demand_2, demand_3
 
 
 def run():
@@ -11,3 +11,5 @@ def run():
     users.run(path, super_shop)
     users_2.run(path, super_shop)
     demand.run(path, super_shop)
+    demand_2.run(path, super_shop)
+    demand_3.run(path, super_shop)
