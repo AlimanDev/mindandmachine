4c3a88af54df1efd5382ff72b45e7bdb8046fb02
ksanvat <ksanworks@gmail.com>
2018-05-01 20:29:47 +0300

    upd

--- src/util/forms.py
+++ src/util/forms.py
@@ -131,8 +131,8 @@ class FormUtil(object):
 
     @staticmethod
     def get_dt_from(form):
-        return DictUtil.get_not_none(form, 'dt_from', datetime.date(year=1971))
+        return DictUtil.get_not_none(form, 'from_dt', datetime.date(year=1971))
 
     @staticmethod
     def get_dt_to(form):
-        return DictUtil.get_not_none(form, 'dt_to', datetime.date(year=2037))
+        return DictUtil.get_not_none(form, 'to_dt', datetime.date(year=2037))
