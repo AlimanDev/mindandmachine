ed4eaf2a4415d78aa6e253441734183019784b9b
ksanvat <ksanworks@gmail.com>
2018-05-04 17:54:49 +0300

    fixes

--- src/main/timetable/cashier_demand/views.py
+++ src/main/timetable/cashier_demand/views.py
@@ -18,7 +18,7 @@ def get_cashiers_timetable(request, form):
 
     if form['format'] == 'excel':
         response = HttpResponse(content_type='application/force-download')
-        response['Content-Disposition'] = 'attachment; filename=Graphic_May.xlsx'
+        response['Content-Disposition'] = 'attachment; filename=May.xlsx'
         response.write(get_xlsx(shop_id=shop.id).read())
         return response
 
