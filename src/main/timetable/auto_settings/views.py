2ac0f103214ae41f34a81ab64cf609e268bebb07
Ciroel <neonx11ant@gmail.com>
2018-05-24 11:46:50 +0300

    better init params for cashiers

--- src/main/timetable/auto_settings/views.py
+++ src/main/timetable/auto_settings/views.py
@@ -204,7 +204,17 @@ def create_timetable(request, form):
             #     'del_day_prob': 0.33
             # },
             {
-                'steps': 2000,
+                'steps': 0,
+                'select_best': 256, # Certalty picking the best initialization # Further params doesn't matter at all
+                'changes': 15,
+                'variety': 8,
+                'days_change_prob': 0.1,
+                'periods_change_prob': 0.55,
+                'add_day_prob': 0.33,
+                'del_day_prob': 0.33
+            },
+            {
+                'steps': 1500,
                 'select_best': 8,
                 'changes': 15,
                 'variety': 8,
