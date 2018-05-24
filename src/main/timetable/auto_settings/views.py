f7abe22abda5144ce8a16bdff37829b85875f07d
Ciroel <neonx11ant@gmail.com>
2018-05-24 17:45:22 +0300

    after merge

--- src/main/timetable/auto_settings/views.py
+++ src/main/timetable/auto_settings/views.py
@@ -356,7 +356,7 @@ def create_timetable(request, form):
             'del_day_prob': 0.33,
         },
         {
-            'steps': 0,
+            'steps': 100,
             'select_best':16,
             'changes': 5,
             'variety': 16,
