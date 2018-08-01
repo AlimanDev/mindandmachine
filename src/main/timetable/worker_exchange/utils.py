c33d5aae5e24c4fccd5872d52cf02bb9a1a7694e
vasiliy <vasili4396@mail.ru>
2018-08-01 18:50:27 +0300

    WIP

--- src/main/timetable/worker_exchange/utils.py
+++ src/main/timetable/worker_exchange/utils.py
@@ -2,21 +2,29 @@ from src.db.models import WorkerDay, WorkerDayCashboxDetails, CashboxType, Worke
 from django.db.models import Q
 
 import datetime as datetime_module
+from datetime import timedelta, time, datetime
+
+from ..cashier_demand.utils import count_diff
 
 CHANGE_TYPE_CHOICES = {
     1: 'FROM OTHER SPEC',
-    2: 'FROM MORN',
-    3: 'FROM EVEN',
-    4: 'EXCESS DAYOFF',
-    5: 'OVERWORKINGS',
-    6: 'FROM OTHER SPEC, 50%',
-    7: 'FROM EVENING IN CASE LESS THAN 5',
-    8: 'DAYOFF'
+    2: 'DAY SWITCH',
+    3: 'EXCESS DAYOFF',
+    4: 'OVERWORKINGS',
+    5: 'FROM OTHER SPEC, 50%',
+    6: 'FROM EVENING IN CASE LESS THAN 5',
+    7: 'DAYOFF'
 }
 
 
 def get_key_by_value(dict_, value):
     return list(dict_.keys())[list(dict_.values()).index(value)]
+
+
+def update_tuple_value(tpl, ind, new_value):
+    lst = list(tpl)
+    lst[ind] = new_value
+    return tuple(lst)
 
 
 def get_cashiers_working_at_time_on(dttm, ct_ids):
@@ -73,3 +81,141 @@ def is_consistent_with_user_constraints(dttm, users):
             return_list.append(user)
 
     return return_list
+
+
+def from_other_spec(shop_id, dttm_exchange, ct_type, predict_demand, mean_bills_per_step, cashbox_types):
+    """
+
+    :param shop_id:
+    :param dttm_exchange:
+    :param ct_type:
+    :param predict_demand:
+    :param mean_bills_per_step:
+    :param cashbox_types:
+    :return: dict = { user : [CHANGE_TYPE, from which cashbox id] }
+    """
+    #presets
+    demand_ind = 0
+    hard_cts_in_shop = []
+    for x in CashboxType.objects.filter(shop_id=shop_id, do_forecast=CashboxType.FORECAST_HARD).values('id').order_by('id'):
+        hard_cts_in_shop.append(x['id'])
+    users_who_can_work_on_ct = get_users_who_can_work_on_ct_type(ct_type)
+
+    #tm settings
+    tm_interval = 60  # minutes
+    standard_tm_interval = 30  # minutes
+    dttm_sections = []
+
+    tm = dttm_exchange - timedelta(minutes=tm_interval / 2)
+    while tm <= dttm_exchange + timedelta(minutes=tm_interval / 2):
+        dttm_sections.append(tm)
+        tm += timedelta(minutes=standard_tm_interval)
+
+    users_for_exchange = {}
+    to_consider = {}  # рассматривать ли вообще тип касс или нет
+    for hard_ct in hard_cts_in_shop:
+        if hard_ct == ct_type:
+            to_consider[hard_ct] = False
+        else:
+            to_consider[hard_ct] = True
+
+    for dttm in dttm_sections:
+        predict_diff_dict, demand_ind = count_diff(dttm, predict_demand, demand_ind, mean_bills_per_step, cashbox_types)
+        users_working_on_hard_cts_at_dttm = get_cashiers_working_at_time_on(dttm, hard_cts_in_shop)  # dict {ct_id: users}
+        for cashbox_type in predict_diff_dict.keys():
+            number_of_workers = len(users_working_on_hard_cts_at_dttm[cashbox_type])
+            if int(predict_diff_dict[cashbox_type]) + 1 < number_of_workers != 1 and to_consider[cashbox_type]:
+                for user in users_working_on_hard_cts_at_dttm[cashbox_type]:
+                    if user in users_who_can_work_on_ct:
+                        if user not in users_for_exchange.keys():
+                            users_for_exchange[user] = []
+                            users_for_exchange[user].append(get_key_by_value(CHANGE_TYPE_CHOICES, 'FROM OTHER SPEC'))
+                            users_for_exchange[user].append(cashbox_type)
+                        else:
+                            if cashbox_type not in users_for_exchange[user]:
+                                users_for_exchange[user].append(cashbox_type)
+
+            else:
+                to_consider[cashbox_type] = False
+
+    return users_for_exchange
+
+
+def day_switch(shop_id, dttm_exchange, ct_type, predict_demand, mean_bills_per_step, cashbox_types):
+    """
+
+    :param shop_id:
+    :param dttm_exchange:
+    :param ct_type:
+    :param predict_demand:
+    :param mean_bills_per_step:
+    :param cashbox_types:
+    :return: dict = { user : [CHANGE_TYPE, from which cashbox id] }
+    """
+    #presets
+    demand_ind = 0
+    hard_cts_in_shop = []
+    for x in CashboxType.objects.filter(shop_id=shop_id, do_forecast=CashboxType.FORECAST_HARD).values('id').order_by('id'):
+        hard_cts_in_shop.append(x['id'])
+    users_who_can_work_on_ct = get_users_who_can_work_on_ct_type(ct_type)
+
+    #tm settings
+    day_begin_dttm = datetime.combine(dttm_exchange.date(), time(6, 30))
+    day_end_dttm = datetime.combine(dttm_exchange.date() + timedelta(days=1), time(2, 0))
+    standard_tm_interval = 30  # minutes
+    amount_of_hours_to_count_transplant = 4  # hours
+
+    users_for_exchange = {}
+    transplant_interval_dict = {}  # { cashbox type : {[dttm1, dttm2] : [users]} }
+    interval_dict = {}  # {(dttm1, dttm2) : users}
+    # to_consider = {}  # рассматривать ли вообще тип касс или нет
+    # for hard_ct in hard_cts_in_shop:
+    #     if hard_ct == ct_type:
+    #         to_consider[hard_ct] = False
+    #     else:
+    #         to_consider[hard_ct] = True
+
+    dttm = day_begin_dttm
+    interval = (dttm, None)
+    interval_dict[interval] = []
+    while dttm <= day_end_dttm:
+        predict_diff_dict, demand_ind = count_diff(dttm, predict_demand, demand_ind, mean_bills_per_step, cashbox_types)
+        users_working_on_hard_cts_at_dttm = get_cashiers_working_at_time_on(dttm, hard_cts_in_shop)  # dict {ct_id: users}
+        # print(interval)
+        for cashbox_type in predict_diff_dict.keys():
+            # if cashbox_type not in transplant_interval_dict.keys():
+            #     transplant_interval_dict[cashbox_type] = interval_dict
+            number_of_workers = len(users_working_on_hard_cts_at_dttm[cashbox_type])
+            # print('ct type : ', cashbox_type, '; dttm : ', dttm, '; interval : ', interval, '; transplant dict : ', transplant_interval_dict[cashbox_type], '; predict : ', int(predict_diff_dict[cashbox_type]) + 1, '; workers : ', number_of_workers)
+            if int(predict_diff_dict[cashbox_type]) + 1 < number_of_workers != 1:
+                # old_interval = interval
+                # new_interval = update_tuple_value(interval, 1, dttm)
+                # print(new_interval, old_interval)
+                # print(interval)
+                # print(interval_dict[interval])
+                # if interval_dict[interval]:
+                #     interval_dict[new_interval] = interval_dict[interval]
+                # else:
+                #     interval_dict[new_interval] = []
+                # del interval_dict[interval]
+                for user in users_working_on_hard_cts_at_dttm[cashbox_type]:
+                    if user in users_who_can_work_on_ct:
+                        if interval not in interval_dict.keys():
+                            interval_dict[interval] = [user]
+                        else:
+                            interval_dict[interval].append(user)
+                        # if user not in users_for_exchange.keys():
+                        #     users_for_exchange[user] = []
+                        #     users_for_exchange[user].append(get_key_by_value(CHANGE_TYPE_CHOICES, 'FROM OTHER SPEC'))
+                        #     users_for_exchange[user].append(cashbox_type)
+                        # else:
+                        #     if cashbox_type not in users_for_exchange[user]:
+                        #         users_for_exchange[user].append(cashbox_type)
+
+            else:
+                if not interval[1] or int((interval[1] - interval[0]).seconds/3600) < amount_of_hours_to_count_transplant:
+                    interval = (dttm, None)
+                # to_consider[cashbox_type] = False
+        dttm += timedelta(minutes=standard_tm_interval)
+
+    return users_for_exchange
--- src/main/timetable/worker_exchange/views.py
+++ src/main/timetable/worker_exchange/views.py
@@ -1,6 +1,3 @@
-from datetime import timedelta, time
-from datetime import datetime
-
 from src.db.models import (
     PeriodDemand,
 )
@@ -9,7 +6,6 @@ from .utils import *
 from src.util.utils import api_method, JsonResponse
 from django.db.models import Max
 
-from ..cashier_demand.utils import count_diff, check_time_is_between_boarders
 from src.util.collection import group_by
 
 
@@ -23,17 +19,7 @@ def get_workers_to_exchange(request, form):
         hard_cts_in_shop.append(x['id'])
 
     users_who_can_work_on_ct = get_users_who_can_work_on_ct_type(ct_type)  # list of users
-    users_for_exchange = {}  # { user obj : [CHANGE_TYPE, cashbox_type_id] }
     users_who_can_be_transplanted = {}  # { timeinterval : users working on this interval }
-
-    tm_interval = 60  # minutes
-    standard_tm_interval = 30  # minutes
-    dttm_sections = []
-
-    tm = dttm_exchange - timedelta(minutes=tm_interval / 2)
-    while tm <= dttm_exchange + timedelta(minutes=tm_interval / 2):
-        dttm_sections.append(tm)
-        tm += timedelta(minutes=standard_tm_interval)
 
     day_begin_dttm = datetime.combine(dttm_exchange.date(), time(6, 30))
     day_end_dttm = datetime.combine(dttm_exchange.date() + timedelta(days=1), time(2, 0))
@@ -63,55 +49,59 @@ def get_workers_to_exchange(request, form):
     while (edge_ind < len(period_demand)) and (period_demand[edge_ind].type != PeriodDemand.Type.FACT.value):
         edge_ind += 1
     predict_demand = period_demand[:edge_ind]
-    demand_ind = 0
 
     cashbox_types_hard = group_by(CashboxType.objects.filter(shop_id=shop_id, do_forecast=CashboxType.FORECAST_HARD).order_by('id'), group_key=lambda x: x.id)
-    change_type = None
-    transplant_interval_dict = {}  # { cashbox type : [[[dttm1, dttm2], [users]], .., ] }
-    amount_of_hours_to_count_transplant = 4
 
-    dttm = day_begin_dttm
-    while dttm <= day_end_dttm:
-        # print(users_for_exchange, dttm)
-        predict_diff_dict, demand_ind = count_diff(dttm, predict_demand, demand_ind, mean_bills_per_step, cashbox_types_hard)
-        users_working_on_hard_cts_at_dttm = get_cashiers_working_at_time_on(dttm, hard_cts_in_shop)  # dict {ct_id: users}
-        for cashbox_type in predict_diff_dict.keys():
-            number_of_workers = len(users_working_on_hard_cts_at_dttm[cashbox_type])
-            print(dttm, ': ', cashbox_type, ': ', int(predict_diff_dict[cashbox_type]) + 1, len(users_working_on_hard_cts_at_dttm[cashbox_type]))
+    # transplant_interval_dict = {}  # { cashbox type : [[[dttm1, dttm2], [users]], .., ] }
+    # amount_of_hours_to_count_transplant = 4
 
-            if cashbox_type not in transplant_interval_dict.keys():
-                transplant_interval_dict[cashbox_type] = []
-            if int(predict_diff_dict[cashbox_type]) + 1 < number_of_workers != 1:
-                if not transplant_interval_dict[cashbox_type]:  # transplant_interval_dict is empty
-                    transplant_interval_dict[cashbox_type].append([[dttm, None], None])  # dttm1
-                elif transplant_interval_dict[cashbox_type] and not transplant_interval_dict[cashbox_type][-1][0][0]:
-                    transplant_interval_dict[cashbox_type][-1][0][0] = dttm
-                else:
-                    transplant_interval_dict[cashbox_type][-1][0][1] = dttm  # dttm2
-                for user in users_working_on_hard_cts_at_dttm[cashbox_type]:
-                    if user in users_who_can_work_on_ct:
-                        if dttm in dttm_sections and cashbox_type != ct_type:
-                            change_type = get_key_by_value(CHANGE_TYPE_CHOICES, 'FROM OTHER SPEC')
-                        if user not in users_for_exchange.keys():
-                            users_for_exchange[user] = []
-                        users_for_exchange[user].append(change_type)
-                        users_for_exchange[user].append(cashbox_type)
-            else:
-                if not transplant_interval_dict[cashbox_type][-1][0][1] or \
-                        int((transplant_interval_dict[cashbox_type][-1][0][1] - transplant_interval_dict[cashbox_type][-1][0][0]).seconds/3600):
-                    transplant_interval_dict[cashbox_type][-1][0][1] = None
-                    transplant_interval_dict[cashbox_type][-1][0][0] = None
-                else:
-                    transplant_interval_dict[cashbox_type].append([[None, None], None])
-                # skip all further intervals as we already know that there is no excess in our interval(morn, even, sections)
-                # if dttm in dttm_sections:
-                #     dttm = dttm_sections[-1]
-                #     users_for_exchange = {k: v for k, v in users_for_exchange.items()
-                #                           if v[0] != get_key_by_value(CHANGE_TYPE_CHOICES, 'FROM OTHER SPEC')}
-        dttm += timedelta(minutes=standard_tm_interval)
-    print(transplant_interval_dict)
+    # dttm = day_begin_dttm
+    # while dttm <= day_end_dttm:
+    #     # print(users_for_exchange, dttm)
+    #     predict_diff_dict, demand_ind = count_diff(dttm, predict_demand, demand_ind, mean_bills_per_step, cashbox_types_hard)
+    #     users_working_on_hard_cts_at_dttm = get_cashiers_working_at_time_on(dttm, hard_cts_in_shop)  # dict {ct_id: users}
+    #     for cashbox_type in predict_diff_dict.keys():
+    #         number_of_workers = len(users_working_on_hard_cts_at_dttm[cashbox_type])
+    #         print(dttm, ': ', cashbox_type, ': ', int(predict_diff_dict[cashbox_type]) + 1, len(users_working_on_hard_cts_at_dttm[cashbox_type]))
+    #         #
+    #         # if cashbox_type not in transplant_interval_dict.keys():
+    #         #     transplant_interval_dict[cashbox_type] = []
+    #         if int(predict_diff_dict[cashbox_type]) + 1 < number_of_workers != 1:
+    #             # if not transplant_interval_dict[cashbox_type]:  # transplant_interval_dict is empty
+    #             #     transplant_interval_dict[cashbox_type].append([[dttm, None], None])  # dttm1
+    #             # elif transplant_interval_dict[cashbox_type] and not transplant_interval_dict[cashbox_type][-1][0][0]:
+    #             #     transplant_interval_dict[cashbox_type][-1][0][0] = dttm
+    #             # else:
+    #             #     transplant_interval_dict[cashbox_type][-1][0][1] = dttm  # dttm2
+    #             for user in users_working_on_hard_cts_at_dttm[cashbox_type]:
+    #                 if user in users_who_can_work_on_ct:
+    #                     if dttm in dttm_sections and cashbox_type != ct_type:
+    #                         change_type = get_key_by_value(CHANGE_TYPE_CHOICES, 'FROM OTHER SPEC')
+    #                     if user not in users_for_exchange.keys():
+    #                         users_for_exchange[user] = []
+    #                     users_for_exchange[user].append(change_type)
+    #                     users_for_exchange[user].append(cashbox_type)
+    #         else:
+    #             pass
+    #             # if not transplant_interval_dict[cashbox_type][-1][0][1] or \
+    #             #         int((transplant_interval_dict[cashbox_type][-1][0][1] - transplant_interval_dict[cashbox_type][-1][0][0]).seconds/3600):
+    #             #     transplant_interval_dict[cashbox_type][-1][0][1] = None
+    #             #     transplant_interval_dict[cashbox_type][-1][0][0] = None
+    #             # else:
+    #             #     transplant_interval_dict[cashbox_type].append([[None, None], None])
+    #             # skip all further intervals as we already know that there is no excess in our interval(morn, even, sections)
+    #             # if dttm in dttm_sections:
+    #             #     dttm = dttm_sections[-1]
+    #             #     users_for_exchange = {k: v for k, v in users_for_exchange.items()
+    #             #                           if v[0] != get_key_by_value(CHANGE_TYPE_CHOICES, 'FROM OTHER SPEC')}
+    #     dttm += timedelta(minutes=standard_tm_interval)
+    # print(transplant_interval_dict)
     # print(is_consistent_with_user_constraints(datetime_module.datetime(2018, 8, 1, 6, 30), users_who_can_work_on_ct))
 
-    # print(users_for_exchange)
+
+
+    # from_other_spec(shop_id, dttm_exchange, ct_type, predict_demand, mean_bills_per_step, cashbox_types_hard)
+
+    day_switch(shop_id, dttm_exchange, ct_type, predict_demand, mean_bills_per_step, cashbox_types_hard)
 
     return JsonResponse.success()
