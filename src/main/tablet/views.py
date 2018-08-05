ee86294d285b52e01065345471e4cf1c61c54604
Alexander Aleskin <alexanderaleskin@mail.ru>
2018-08-05 23:29:21 +0300

    fix users cashbox_types

--- src/main/tablet/views.py
+++ src/main/tablet/views.py
@@ -219,7 +219,7 @@ def get_cashiers_info(request, form):
             })
 
     user_ids = response.keys()
-    worker_cashboxes_types = WorkerCashboxInfo.objects.select_related('cashbox_type').filter(worker_id__in=user_ids)
+    worker_cashboxes_types = WorkerCashboxInfo.objects.select_related('cashbox_type').filter(worker_id__in=user_ids, is_active=True)
     worker_cashboxes_types = group_by(list(worker_cashboxes_types), group_key=lambda _: _.worker_id,)
     for user_id in response.keys():
         response[user_id]['cashbox_types'] = [WorkerCashboxInfoConverter.convert(x) for x in worker_cashboxes_types[user_id]]
