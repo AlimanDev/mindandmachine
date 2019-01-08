822a572030ba2dac7a1f4c77188f7551fa0af94e
vasiliy <vasili4396@mail.ru>
2019-01-08 22:29:58 +0300

    fixed

--- src/db/works/fill_production_calendar/fill_calendar.py
+++ src/db/works/fill_production_calendar/fill_calendar.py
@@ -6,12 +6,13 @@ from src.db.works.fill_production_calendar import fill_calendar
 
 Examples:
     fill_calendar.fill_month(2018, 2019) -- only for months
-    fill_calendar.fill_days('2018.1.1', '2019.1.1')  -- onlu for production days
+    fill_calendar.fill_days('2018.1.1', '2019.1.1')  -- only for production days
     fill_calendar.main('2018.1.1', '2019.1.1') -- for both months and days
 
 """
 import pandas as pd
 import datetime
+from src.util.months import month_dict
 from calendar import monthrange
 from src.db.models import (
     ProductionMonth,
@@ -30,33 +31,17 @@ def fill_months(from_year, to_year):
          from_year(int): e.g. 2018
          to_year(int): e.g. 2020
     """
-    file = pd.read_csv(
+    data = pd.read_csv(
         'src/db/works/fill_production_calendar/work_data.csv',
-        index_col=False,
-        sep=';',
-        names=['dts', 'types']
+        index_col='Год/Месяц'
     )
-    data = file['dts']
-    from_year_index, to_year_index = None, None
-    for year_index in range(1, data.shape[0]):
-        if int(data[year_index].split(',')[0]) == from_year:
-            from_year_index = year_index
-        if int(data[year_index].split(',')[0]) == to_year:
-            to_year_index = year_index
-    if not from_year_index or not to_year_index:
-        raise ValueError('не смог определить от какого до какого года работать.')
 
-    for index in range(from_year_index, to_year_index + 1):
-        year_data = data[index].split(',"')
-        year = int(year_data[0])  # e.g. 2013
-        for month in range(1, 13):
-            month_data = year_data[month].split(',')
-            if month == 12:
-                last_day_index = month_data.index([day for day in month_data if '"' in day][0])
-                month_data = month_data[:last_day_index + 1]
-            month_data[-1] = month_data[-1].replace('"', '')
-            dt_first = datetime.date(year, month, 1)
-            total_days = monthrange(year, month)[1]
+    for year in range(from_year, to_year + 1):
+        year_data = data.loc[year]
+        for month_num, month_name in month_dict.items():
+            month_data = year_data[month_name.title()].split(',')
+            dt_first = datetime.date(year, month_num, 1)
+            total_days = monthrange(year, month_num)[1]
             short_days = len([day for day in month_data if '*' in day])
             norm_work_days = total_days - len(month_data) + short_days
             norm_work_hours = norm_work_days * 8 - short_days  # один день со звездочкой -- 7часовой рабочий день
@@ -77,40 +62,22 @@ def fill_days(from_date, to_date):
         from_date(str): e.g. '2018.1.1'
         to_date(str): e.g. '2019.1.1'
     """
-    file = pd.read_csv(
+    data = pd.read_csv(
         'src/db/works/fill_production_calendar/work_data.csv',
-        index_col=False,
-        sep=';',
-        names=['dts', 'types']
+        index_col='Год/Месяц'
     )
-    data = file['dts']
-    from_year_index, to_year_index = None, None
-    from_date = datetime.datetime.strptime(from_date, '%Y.%m.%d').date()
-    to_date = datetime.datetime.strptime(to_date, '%Y.%m.%d').date()
-    for year_index in range(1, data.shape[0]):
-        if int(data[year_index].split(',')[0]) == from_date.year:
-            from_year_index = year_index
-        if int(data[year_index].split(',')[0]) == to_date.year:
-            to_year_index = year_index
-    if not from_year_index or not to_year_index:
-        raise ValueError('не смог определить от какого до какого года работать.')
 
-    for index in range(from_year_index, to_year_index + 1):
-        year_data = data[index].split(',"')
-        year = int(year_data[0])  # e.g. 2013
-        for month in range(1, 13):
-            month_data = year_data[month].split(',')
-            if month == 12:
-                last_day_index = month_data.index([day for day in month_data if '"' in day][0])
-                month_data = month_data[:last_day_index + 1]
-            month_data[-1] = month_data[-1].replace('"', '')
-            total_days = monthrange(year, month)[1]
-            short_days = [day for day in month_data if '*' in day]
-            short_days = [no_star.replace('*', '') for no_star in short_days]
+    from_year = int(from_date.split('.')[0])
+    to_year = int(to_date.split('.')[0])
+
+    for year in range(from_year, to_year + 1):
+        year_data = data.loc[year]
+        for month_num, month_name in month_dict.items():
+            month_data = year_data[month_name.title()].split(',')
+            total_days = monthrange(year, month_num)[1]
+            short_days = [day.replace('*', '') for day in month_data if '*' in day]
             for day in range(total_days):
-                if datetime.date(year, month, day + 1) > to_date:
-                    break
-                dt = datetime.date(year, month, day + 1)
+                dt = datetime.date(year, month_num, day + 1)
                 if str(dt.day) in short_days:
                     day_type = ProductionDay.TYPE_SHORT_WORK
                 elif str(dt.day) in month_data:
@@ -124,13 +91,6 @@ def fill_days(from_date, to_date):
                         'is_celebration': False
                     }
                 )
-
-            else:
-                continue
-            break
-        else:
-            continue
-        break
 
 
 def main(from_date, to_date):
