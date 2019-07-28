ddb3628b23d044e6f524156582db642c9b4218c3
Hexagon000 <40472929+Hexagon000@users.noreply.github.com>
2019-07-28 12:43:23 +0300

    Fix tests after 'Exchange fixes'. (#144)

--- src/main/timetable/worker_exchange/tests.py
+++ src/main/timetable/worker_exchange/tests.py
@@ -1,14 +1,21 @@
 from src.util.test import LocalTestCase
 from src.db.models import (
+    ExchangeSettings,
+    SuperShop,
     WorkType,
     WorkerDayCashboxDetails,
     Shop,
     User,
     WorkerDay,
+    OperationType,
+    PeriodClients,
+    Event,
 )
 import datetime
 from django.utils.timezone import now
 from dateutil.relativedelta import relativedelta
+from django.test import TestCase
+from .utils import create_vacancies_and_notify, cancel_vacancies, workers_exchange
 
 
 class TestWorkerExchange(LocalTestCase):
@@ -148,3 +155,258 @@ class TestWorkerExchange(LocalTestCase):
         self.assertEqual(type(WorkerDayCashboxDetails.objects.get(pk=worker_day_detail.id).dttm_deleted),
                          type(datetime.datetime.now()))
         self.assertEqual(WorkerDayCashboxDetails.objects.get(pk=worker_day_detail2.id).dttm_deleted, None)
+
+
+class Test_auto_worker_exchange(TestCase):
+    dt_now = now().date()
+
+    def setUp(self):
+        super().setUp()
+
+        self.superShop = SuperShop.objects.create(
+            title='SuperShop1',
+            tm_start=datetime.time(7, 0, 0),
+            tm_end=datetime.time(0, 0, 0)
+        )
+
+        self.shop = Shop.objects.create(
+            super_shop=self.superShop,
+            title='Shop1'
+        )
+
+        self.shop2 = Shop.objects.create(
+            super_shop=self.superShop,
+            title='Shop2'
+        )
+
+        self.work_type = WorkType.objects.create(
+            shop=self.shop,
+            name='Кассы'
+        )
+
+        self.work_type2 = WorkType.objects.create(
+            shop=self.shop2,
+            name='Кассы'
+        )
+
+        self.operation_type = OperationType.objects.create(
+            name='operation type №1',
+            work_type=self.work_type,
+            do_forecast=OperationType.FORECAST_HARD
+        )
+
+        self.operation_type2 = OperationType.objects.create(
+            name='operation type №1',
+            work_type=self.work_type2,
+            do_forecast=OperationType.FORECAST_HARD
+        )
+
+        self.exchange_settings = ExchangeSettings.objects.create(
+            automatic_check_lack_timegap=datetime.timedelta(days=1),
+            automatic_check_lack=True,
+            automatic_create_vacancy_lack_min=0.4,
+            automatic_delete_vacancy_lack_max=0.5,
+            automatic_worker_select_overflow_min=0.6,
+            automatic_worker_select_timegap=datetime.timedelta(hours=4)
+        )
+
+    def create_vacancy(self, tm_from, tm_to):
+        WorkerDayCashboxDetails.objects.create(
+            dttm_from=('{} ' + tm_from).format(self.dt_now),
+            dttm_to=('{} ' + tm_to).format(self.dt_now),
+            work_type=self.work_type,
+            status=WorkerDayCashboxDetails.TYPE_VACANCY,
+            is_vacancy=True
+        )
+
+    def create_period_clients(self, value, operation_type):
+        dttm_from = now().replace(hour=9, minute=0, second=0, microsecond=0)
+        dttm_to = dttm_from.replace(hour=21, minute=0, second=0, microsecond=0)
+        pc_list = []
+        while dttm_from < dttm_to:
+            pc_list.append(PeriodClients(
+                dttm_forecast=dttm_from,
+                value=value,
+                type=PeriodClients.LONG_FORECASE_TYPE,
+                operation_type=operation_type
+            ))
+            dttm_from += datetime.timedelta(minutes=30)
+        else:
+            PeriodClients.objects.bulk_create(pc_list)
+
+    def create_users(self, quantity):
+        for number in range(1, quantity + 1):
+            User.objects.create_user(
+                username='User{}'.format(number),
+                email='test{}@test.ru'.format(number),
+                shop=self.shop2,
+                last_name='Имя{}'.format(number),
+                first_name='Фамилия{}'.format(number)
+            )
+
+    def create_worker_day(self):
+        for user in User.objects.all():
+            wd = WorkerDay.objects.create(
+                worker=user,
+                dt=self.dt_now,
+                type=WorkerDay.Type.TYPE_WORKDAY.value,
+                dttm_work_start='{} 09:00:00'.format(self.dt_now),
+                dttm_work_end='{} 21:00:00'.format(self.dt_now),
+            )
+
+            WorkerDayCashboxDetails.objects.create(
+                dttm_from='{} 09:00:00'.format(self.dt_now),
+                dttm_to='{} 21:00:00'.format(self.dt_now),
+                work_type=self.work_type2,
+                status=WorkerDayCashboxDetails.TYPE_WORK,
+                is_vacancy=False,
+                worker_day=wd
+            )
+
+    # Создали прогноз PeriodClients -> нужен 1 человек (1 вакансия), а у нас их 2 -> удаляем 1 вакансию
+    def test_cancel_vacancies(self):
+        self.create_vacancy('09:00:00', '20:00:00')
+        self.create_vacancy('09:00:00', '20:00:00')
+
+        self.create_period_clients(40, self.operation_type)
+
+        wdcd = WorkerDayCashboxDetails.objects.all()
+        self.assertEqual(len(wdcd.filter(status=WorkerDayCashboxDetails.TYPE_VACANCY)), 2)
+
+        cancel_vacancies(self.shop, self.work_type)
+
+        wdcd = WorkerDayCashboxDetails.objects.all()
+        self.assertEqual(len(wdcd.filter(status=WorkerDayCashboxDetails.TYPE_DELETED)), 1)
+        self.assertEqual(len(wdcd.filter(status=WorkerDayCashboxDetails.TYPE_VACANCY)), 1)
+
+    # Нужны 3 вакансии -> у нас 0 -> создаём 3
+    def test_create_vacancies_and_notify(self):
+        self.create_period_clients(72, self.operation_type)
+
+        len_wdcd = len(WorkerDayCashboxDetails.objects.filter(status=WorkerDayCashboxDetails.TYPE_VACANCY))
+        self.assertEqual(len_wdcd, 0)
+        create_vacancies_and_notify(self.shop, self.work_type)
+        wdcd = WorkerDayCashboxDetails.objects.filter(status=WorkerDayCashboxDetails.TYPE_VACANCY).order_by('dttm_to')
+        print(wdcd.count(), '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+        self.assertEqual([wdcd[0].dttm_from.time(), wdcd[0].dttm_to.time()],
+                         [datetime.time(9, 0), datetime.time(21, 0)])
+        self.assertEqual([wdcd[1].dttm_from.time(), wdcd[1].dttm_to.time()],
+                         [datetime.time(9, 0), datetime.time(21, 0)])
+        self.assertEqual([wdcd[2].dttm_from.time(), wdcd[2].dttm_to.time()],
+                         [datetime.time(9, 0), datetime.time(21, 0)])
+
+    # Нужно 3 вакансии -> у нас есть 2 -> нужно создать 1
+    def test_create_vacancies_and_notify2(self):
+        self.create_vacancy('09:00:00', '20:00:00')
+        self.create_vacancy('09:00:00', '20:00:00')
+
+        self.create_period_clients(72, self.operation_type)
+
+        len_wdcd = len(WorkerDayCashboxDetails.objects.filter(status=WorkerDayCashboxDetails.TYPE_VACANCY))
+        self.assertEqual(len_wdcd, 2)
+        create_vacancies_and_notify(self.shop, self.work_type)
+        wdcd = WorkerDayCashboxDetails.objects.filter(status=WorkerDayCashboxDetails.TYPE_VACANCY).order_by('dttm_to')
+        self.assertEqual([wdcd[0].dttm_from.time(), wdcd[0].dttm_to.time()],
+                         [datetime.time(9, 0), datetime.time(20, 0)])
+        self.assertEqual([wdcd[1].dttm_from.time(), wdcd[1].dttm_to.time()],
+                         [datetime.time(9, 0), datetime.time(20, 0)])
+        self.assertEqual([wdcd[2].dttm_from.time(), wdcd[2].dttm_to.time()],
+                         [datetime.time(9, 0), datetime.time(21, 0)])
+
+    # Есть вакансия с 12-17, создаёт 2 доп. 1. 9-13; 2. 17-21
+    def test_create_vacancies_and_notify3(self):
+        self.create_vacancy('12:00:00', '17:00:00')
+
+        self.create_period_clients(18, self.operation_type)
+
+        len_wdcd = len(WorkerDayCashboxDetails.objects.filter(status=WorkerDayCashboxDetails.TYPE_VACANCY))
+        self.assertEqual(len_wdcd, 1)
+        create_vacancies_and_notify(self.shop, self.work_type)
+        wdcd = WorkerDayCashboxDetails.objects.filter(status=WorkerDayCashboxDetails.TYPE_VACANCY).order_by('dttm_to')
+        self.assertEqual([wdcd[0].dttm_from.time(), wdcd[0].dttm_to.time()], [datetime.time(9, 0),
+                                                                              datetime.time(13, 0)])
+        self.assertEqual([wdcd[1].dttm_from.time(), wdcd[1].dttm_to.time()], [datetime.time(12, 0),
+                                                                              datetime.time(17, 0)])
+        self.assertEqual([wdcd[2].dttm_from.time(), wdcd[2].dttm_to.time()], [datetime.time(17, 0),
+                                                                              datetime.time(21, 0)])
+
+    # Есть 2 вакансии 9-14 и 16-21. Создаётся 3ая с 14-18
+    def test_create_vacancies_and_notify4(self):
+        self.create_vacancy('09:00:00', '14:00:00')
+        self.create_vacancy('16:00:00', '21:00:00')
+
+        self.create_period_clients(18, self.operation_type)
+
+        len_wdcd = len(WorkerDayCashboxDetails.objects.filter(status=WorkerDayCashboxDetails.TYPE_VACANCY))
+        self.assertEqual(len_wdcd, 2)
+        create_vacancies_and_notify(self.shop, self.work_type)
+        wdcd = WorkerDayCashboxDetails.objects.filter(status=WorkerDayCashboxDetails.TYPE_VACANCY).order_by('dttm_to')
+        self.assertEqual([wdcd[0].dttm_from.time(), wdcd[0].dttm_to.time()], [datetime.time(9, 0),
+                                                                              datetime.time(14, 0)])
+        self.assertEqual([wdcd[1].dttm_from.time(), wdcd[1].dttm_to.time()], [datetime.time(14, 0),
+                                                                              datetime.time(18, 0)])
+        self.assertEqual([wdcd[2].dttm_from.time(), wdcd[2].dttm_to.time()], [datetime.time(16, 0),
+                                                                              datetime.time(21, 0)])
+
+    # Есть 2 вакансии 9-15 и 16-21. Ничего не создаётся - разница между вакансиями < working_shift_min_hours / 2
+    def test_create_vacancies_and_notify5(self):
+        self.create_vacancy('09:00:00', '15:00:00')
+        self.create_vacancy('16:00:00', '21:00:00')
+
+        self.create_period_clients(18, self.operation_type)
+
+        len_wdcd = len(WorkerDayCashboxDetails.objects.filter(status=WorkerDayCashboxDetails.TYPE_VACANCY))
+        self.assertEqual(len_wdcd, 2)
+        create_vacancies_and_notify(self.shop, self.work_type)
+        len_wdcd = len(WorkerDayCashboxDetails.objects.filter(status=WorkerDayCashboxDetails.TYPE_VACANCY))
+        self.assertEqual(len_wdcd, 2)
+
+    # Предикшн в 3 человека -> 4 человека в работе -> 1 перекидывает.
+    def test_workers_hard_exchange(self):
+        self.create_users(4)
+        self.create_worker_day()
+
+        self.create_period_clients(18, self.operation_type)
+        self.create_period_clients(72, self.operation_type2)
+
+        self.create_vacancy('09:00:00', '21:00:00')
+        Event.objects.create(
+            text='Ивент для тестов, вакансия id 5.',
+            department=self.shop,
+            workerday_details=WorkerDayCashboxDetails.objects.get(pk=5)
+        )
+
+        wdcd = WorkerDayCashboxDetails.objects.all()
+        self.assertEqual(len(wdcd), 5)
+        self.assertEqual(len(wdcd.filter(pk=1)), 1)
+        self.assertEqual(wdcd.get(pk=5).status, WorkerDayCashboxDetails.TYPE_VACANCY)
+
+        workers_exchange()
+
+        wdcd = WorkerDayCashboxDetails.objects.all()
+        self.assertEqual(len(wdcd), 4)
+        self.assertEqual(len(wdcd.filter(pk=1)), 0)
+        self.assertEqual(wdcd.get(pk=5).status, WorkerDayCashboxDetails.TYPE_WORK)
+
+    # Предикшн в 4 человека -> 4 человека в работе -> никого не перекидывает.
+    def test_workers_hard_exchange2(self):
+        self.create_users(4)
+        self.create_worker_day()
+
+        self.create_period_clients(18, self.operation_type)
+        self.create_period_clients(150, self.operation_type2)
+
+        self.create_vacancy('09:00:00', '21:00:00')
+        Event.objects.create(
+            text='Ивент для тестов, вакансия id 5.',
+            department=self.shop,
+            workerday_details=WorkerDayCashboxDetails.objects.get(pk=5)
+        )
+
+        wdcd = WorkerDayCashboxDetails.objects.all()
+        self.assertEqual(len(wdcd), 5)
+
+        workers_exchange()
+
+        wdcd = WorkerDayCashboxDetails.objects.all()
+        self.assertEqual(len(wdcd), 5)
