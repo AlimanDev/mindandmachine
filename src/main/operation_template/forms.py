d64fb735af44628c190699a06903770ce53e8df3
Popova Anastassiya <anastassiya.popova@gmail.com>
2019-09-11 18:20:06 +0300

    use ModelForm

--- src/main/operation_template/forms.py
+++ src/main/operation_template/forms.py
@@ -1,32 +1,27 @@
 from django import forms
 from src.util import forms as util_forms
+from src.db.models import OperationTemplate
 
 
 class GetOperationTemplatesForm(forms.Form):
     shop_id = forms.IntegerField(required=False)
-    operation_type_id = forms.IntegerField()
-
-
-class CreateOperationTemplateForm(forms.Form):
-    operation_type_id = forms.IntegerField()
-    name = forms.CharField(max_length=128)
-    value = forms.FloatField()
-    tm_start = util_forms.TimeField()
-    tm_end = util_forms.TimeField()
-    period = util_forms.ChoiceField(choices=['D', 'W', 'M'])
-    days_in_period = util_forms.IntegersList()
+    operation_type_id = forms.IntegerField(required=False)
 
 
 class DeleteOperationTemplateForm(forms.Form):
     id = forms.IntegerField()
 
 
-class UpdateOperationTemplateForm(forms.Form):
-    id = forms.IntegerField()
-    name = forms.CharField(max_length=128)
-    value = forms.FloatField()
-    tm_start = util_forms.TimeField()
-    tm_end = util_forms.TimeField()
-    period = util_forms.ChoiceField(choices=['D', 'W', 'M'])
-    days_in_period = util_forms.IntegersList()
+class OperationTemplateForm(forms.ModelForm):
+    class Meta:
+        model = OperationTemplate
+        fields = ['name', 'value', 'tm_start', 'tm_end', 'period', 'days_in_period']
+
+
+class CreateOperationTemplateForm(OperationTemplateForm):
+    operation_type_id = forms.IntegerField(required=False)
+
+
+class UpdateOperationTemplateForm(OperationTemplateForm):
+    id = forms.IntegerField(required=True)
     date_rebuild_from = forms.DateField(required=False)
