dc32fb2b184e856627f681746cd300216047e35b
Popova Anastassiya <anastassiya.popova@gmail.com>
2019-09-18 11:59:35 +0300

    tests

--- src/main/operation_template/tests.py
+++ src/main/operation_template/tests.py
@@ -41,18 +41,28 @@ class TestOperationTemplate(LocalTestCase):
             value=2.25
         )
     def create_period_clients(self, dt_from, dt_to):
+        creates = []
         while dt_from <= dt_to:
-            tm_start = datetime.combine(dt_from,  time(11))
-            tm_end = datetime.combine(dt_from,  time(19, 30))
-            while tm_start <= tm_end:
-                PeriodClients.objects.create(
-                    dttm_forecast=tm_start,
-                    value=1,
-                    type=PeriodClients.LONG_FORECASE_TYPE,
-                    operation_type=self.operation_type
-                )
-                tm_start += timedelta(minutes=30)
+            creates += self.create_period_clients_tm(
+                datetime.combine(dt_from,  time(9)),
+                datetime.combine(dt_from,  time(10)))
+            creates += self.create_period_clients_tm(
+                datetime.combine(dt_from,  time(11, 30)),
+                datetime.combine(dt_from,  time(18, 30)))
             dt_from += timedelta(days=1)
+        PeriodClients.objects.bulk_create(creates)
+
+    def create_period_clients_tm(self, tm_start, tm_end):
+        creates = []
+        while tm_start <= tm_end:
+            creates.append(PeriodClients(
+                dttm_forecast=tm_start,
+                value=1,
+                type=PeriodClients.LONG_FORECASE_TYPE,
+                operation_type=self.operation_type
+            ))
+            tm_start += timedelta(minutes=30)
+        return creates
 
     def test_generate_dates(self):
         dt_from = self.dt_from
@@ -68,7 +78,7 @@ class TestOperationTemplate(LocalTestCase):
         self.assertEqual(len(dates), 15 * 5)
 
     def test_build_period_clients_week(self):
-        total_days = 15
+        total_days = 14
         ot_days = 6
         times = 4 # периодов в день
         dt_from = datetime.now().date() + timedelta(days=5) #  15 дней
@@ -184,6 +194,61 @@ class TestOperationTemplate(LocalTestCase):
             value=1
         )
         self.assertEqual(len(pc), 62 * 18)
+
+        pc=PeriodClients.objects.filter(
+            value=0
+        )
+        self.assertEqual(len(pc), ot_days * 2)
+
+    def test_build_period_clients_day(self):
+        total_days = 30 # дней за период
+        ot_days = total_days # дней по шаблону
+        times = 4 # периодов в день
+        dt_from = datetime.now().date() + timedelta(days=5)
+        dt_to = dt_from + timedelta(days=total_days-1)
+
+        self.create_period_clients(dt_from, dt_to)
+        pc = PeriodClients.objects.all()
+        self.assertEqual(len(pc), total_days * 18)
+
+        # create by operation_template
+        utils.build_period_clients(self.ot_daily, dt_from, dt_to)
+
+        self.assertEqual(len(PeriodClients.objects.all()), total_days * 18 + ot_days * 2)
+
+        #по часу в день - увеличено
+        pc=PeriodClients.objects.filter(
+            value=3.25
+        )
+        self.assertEqual(len(pc), ot_days * times / 2 )
+
+        #по часу в день  - добавлено
+        pc=PeriodClients.objects.filter(
+            value=2.25
+        )
+        self.assertEqual(len(pc), ot_days * times / 2)
+
+
+        pc=PeriodClients.objects.filter(
+            value__gt=1
+        ).order_by('dttm_forecast')
+        dates = pc.values_list('dttm_forecast', flat=True)
+
+        self.assertEqual(dates[0].time(), time(10, 0))
+        self.assertEqual(dates[5].time(), time(10, 30))
+        self.assertEqual(dates[10].time(), time(11, 0))
+
+        # delete by operation_template
+        utils.build_period_clients(
+            self.ot_daily,
+            dt_from,
+            dt_to,
+            operation='delete')
+
+        pc=PeriodClients.objects.filter(
+            value=1
+        )
+        self.assertEqual(len(pc), total_days * 18)
 
         pc=PeriodClients.objects.filter(
             value=0
