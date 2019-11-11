24f81fad07c40686eb853eeb67eb49a3f870cfe4
Anton <antosha250900@gmail.com>
2019-11-11 15:26:40 +0300

    Fixed working_hours with max function

--- src/main/download/xlsx/tabel.py
+++ src/main/download/xlsx/tabel.py
@@ -362,7 +362,7 @@ class Tabel_xlsx(Xlsx_base):
                     self.workbook.add_format(cell_format)
                 )
                 dt = self.month.replace(day=day+1)
-                user_working_hours = round(working_hours.get(user.id, {}).get(dt, 0))
+                user_working_hours = working_hours.get(user.id, {}).get(dt, 0)
                 breaktime = 0
                 for triplet in triplets:
                     if triplet[0] <= user_working_hours and triplet[1] >= user_working_hours:
@@ -370,7 +370,7 @@ class Tabel_xlsx(Xlsx_base):
                 self.worksheet.write(
                     row_s + row_shift * 2 + 1,
                     col_s + day,
-                    user_working_hours - breaktime if user_working_hours > 0 else 0,
+                    round(max(user_working_hours - breaktime, 0)),
                     self.workbook.add_format(cell_format)
                 )
 
