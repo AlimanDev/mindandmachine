d3d9f0eed8d17084d9554b4bf41d9865ee9aec8e
Popova Anastassiya <anastassiya.popova@gmail.com>
2020-01-10 15:38:26 +0300

    user api fix

--- src/base/views.py
+++ src/base/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 
-from src.base.permissions import FilteredListPermission
+from src.base.permissions import FilteredListPermission, Permission
 from src.base.serializers import EmploymentSerializer, UserSerializer
 from src.base.filters import EmploymentFilter, UserFilter
 
@@ -16,14 +16,8 @@ class EmploymentViewSet(viewsets.ModelViewSet):
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    permission_classes = [FilteredListPermission]
+    permission_classes = [Permission]
     serializer_class = UserSerializer
     filterset_class = UserFilter
 
-    def get_queryset(self):
-        shop_id = self.request.query_params.get('shop_id')
-
-        employments = Employment.objects \
-            .get_active(shop_id=shop_id)
-        return User.objects.filter(id__in=employments.values('user_id'))
-
+    queryset = User.objects.all()
