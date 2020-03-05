0bf992804f77671399276c0085a3f311debfb15e
Popova Anastassiya <anastassiya.popova@gmail.com>
2020-03-05 11:39:25 +0300

    fix base migrations

--- src/base/migrations/0009_auto_20200211_0723.py
+++ src/base/migrations/0009_auto_20200211_0723.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0008_auto_20200205_0924'),
+        ('base', '0008_fix'),
     ]
 
     operations = [
