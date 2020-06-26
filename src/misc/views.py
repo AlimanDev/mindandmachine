1e48a433fd4ed6fac2f764523ff973f0fcac453d
Popova Anastassiya <anastassiya.popova@gmail.com>
2020-06-26 17:49:45 +0300

    create metabase url

---
+++ src/misc/views.py
@@ -0,0 +1,26 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.conf import settings
+import jwt
+import time
+
+METABASE_SITE_URL = settings.METABASE_SITE_URL
+METABASE_SECRET_KEY = settings.METABASE_SECRET_KEY
+
+
+@api_view()
+def metabase_url(request):
+    # You'll need to install PyJWT via pip 'pip install PyJWT' or your project packages file
+
+
+    payload = {
+        "resource": {"dashboard": 3},
+        "params": {
+
+        },
+        "exp": round(time.time()) + (60 * 60)  # 1 hour expiration
+    }
+    token = jwt.encode(payload, METABASE_SECRET_KEY, algorithm="HS256")
+
+    iframeUrl = METABASE_SITE_URL + "/embed/dashboard/" + token.decode("utf8") + "#bordered=true&titled=true"
+    return Response({"url": iframeUrl})
