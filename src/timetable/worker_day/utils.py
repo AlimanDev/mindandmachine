bfdfd57a431db2512f300aa425f6a9da4619b3f7
Anton <antosha250900@gmail.com>
2020-10-02 10:47:46 +0300

    fixed download tt

--- src/timetable/worker_day/utils.py
+++ src/timetable/worker_day/utils.py
@@ -282,14 +282,15 @@ def download_timetable_util(request, workbook, form):
         dt_to=timetable.prod_days[-1].dt,
         shop=shop,
     ).order_by('position_id', 'user__last_name', 'user__first_name', 'user__middle_name', 'tabel_code', 'id')
+    users = employments.values_list('user_id', flat=True)
 
     breaktimes = json.loads(shop.settings.break_triplets)
     breaktimes = list(map(lambda x: (x[0] / 60, x[1] / 60, sum(x[2]) / 60), breaktimes))
 
     workdays = WorkerDay.objects.select_related('worker', 'shop').filter(
-        Q(dt__lt=F('employment__dt_fired')) | Q(employment__dt_fired__isnull=True),
-        Q(dt__gte=F('employment__dt_hired')) & Q(dt__gte=timetable.prod_days[0].dt),
-        employment__in=employments,
+        Q(dt__lt=F('employment__dt_fired')) | Q(employment__dt_fired__isnull=True) | Q(employment__isnull=True),
+        (Q(dt__gte=F('employment__dt_hired')) | Q(employment__isnull=True)) & Q(dt__gte=timetable.prod_days[0].dt),
+        worker__in=users,
         dt__lte=timetable.prod_days[-1].dt,
         is_approved=form['is_approved'],
         is_fact=False,
