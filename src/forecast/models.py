bd5e681fad393f12de332b545e70ab602716223c
Anton <antosha250900@gmail.com>
2020-11-12 15:32:20 +0300

    fixed Q

--- src/forecast/models.py
+++ src/forecast/models.py
@@ -296,9 +296,9 @@ class PeriodClientsManager(models.Manager):
             6: 1,
         }
         if weekday and not shop.open_times.get('all', False):
-            filt = models.Q(False)
+            filt = models.Q()
             for k, v in shop.open_times.items():
-                filt |= models.Q(dttm_forecast__week_day=weekdays_db[int(k)]) & (models.Q(dttm_forecast__time__gte=v) | models.Q(dttm_forecast__time__lt=shop.close_times[k]))
+                filt |= (models.Q(dttm_forecast__week_day=weekdays_db[int(k)]) & (models.Q(dttm_forecast__time__gte=v) | models.Q(dttm_forecast__time__lt=shop.close_times[k])))
             return self.filter(filt, *args, **kwargs)
         else:
             max_shop_time = max(list(shop.close_times.values()))
