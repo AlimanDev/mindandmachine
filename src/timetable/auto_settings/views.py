1821e9769364b7f4e877ccf2011bc32ae1d101c1
Dmitriev <dmitriev_as@srvhub.ru>
2020-11-13 10:27:31 +0300

    HOST_IP -> HOST

--- src/timetable/auto_settings/views.py
+++ src/timetable/auto_settings/views.py
@@ -696,7 +696,7 @@ class AutoSettingsViewSet(viewsets.ViewSet):
         }
         breaks['default'] = shop.settings.breaks.breaks
         data = {
-            'IP': settings.HOST_IP,
+            'IP': settings.HOST,
             'timetable_id': tt.id,
             'forecast_step_minutes': shop.forecast_step_minutes.minute,
             'work_types': list(work_types.values()),
