d514225fe52383443db8c49dd6fd4cefce13e863
Dmitriev <dmitriev_as@srvhub.ru>
2020-11-20 15:40:03 +0300

    mts wd perms preset

--- src/util/tests/test_wd_perms_presets.py
+++ src/util/tests/test_wd_perms_presets.py
@@ -3,7 +3,9 @@ from django.test import TestCase
 from src.base.models import Group
 from src.timetable.models import GroupWorkerDayPermission
 from src.util.mixins.tests import TestsHelperMixin
-from src.util.wd_perms.utils import AdminPreset, URSOrtekaPreset, DirectorOrtekaPreset, EmptyPreset
+from src.util.wd_perms.utils import (
+    AdminPreset, URSOrtekaPreset, DirectorOrtekaPreset, EmptyPreset, DirectorAndURSMtsPreset
+)
 
 
 class TestWdPermsPresets(TestsHelperMixin, TestCase):
@@ -19,5 +21,6 @@ class TestWdPermsPresets(TestsHelperMixin, TestCase):
     def test_wd_perms(self):
         self._test_wd_perm(AdminPreset, 66)
         self._test_wd_perm(URSOrtekaPreset, 66)
-        self._test_wd_perm(DirectorOrtekaPreset, 33)
+        self._test_wd_perm(DirectorOrtekaPreset, 35)
+        self._test_wd_perm(DirectorAndURSMtsPreset, 30)
         self._test_wd_perm(EmptyPreset, 0)
