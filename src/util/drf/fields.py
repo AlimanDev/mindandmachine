fe13885986d8c9da4da6b30609c5ad5869acd2af
Dmitriev <dmitriev_as@srvhub.ru>
2020-11-26 18:56:46 +0300

    RoundingDecimalField comment

--- src/util/drf/fields.py
+++ src/util/drf/fields.py
@@ -2,5 +2,10 @@ from rest_framework import serializers
 
 
 class RoundingDecimalField(serializers.DecimalField):
+    """
+    Класс DecimalField с отключенной проверкой точности.
+    Чтобы можно было принимать числа с большим количество знаков после запятой
+    (при сохранении они будут округляться до нужной точности)
+    """
     def validate_precision(self, value):
         return value
