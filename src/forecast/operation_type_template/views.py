3679d906e78a20acc9698884181b6a9f8392a69d
Anton250 <antosha250900@gmail.com>
2020-12-15 10:35:25 +0300

    workload - added check forecast step on operation type template

--- src/forecast/operation_type_template/views.py
+++ src/forecast/operation_type_template/views.py
@@ -5,9 +5,19 @@ from src.forecast.models import OperationTypeTemplate, LoadTemplate
 from src.forecast.operation_type_name.views import OperationTypeNameSerializer
 from rest_framework.validators import UniqueTogetherValidator
 from src.base.permissions import Permission
+from src.base.exceptions import FieldError
+
+from django.utils.translation import gettext_lazy as _
+
+from datetime import timedelta
 
 # Serializers define the API representation.
 class OperationTypeTemplateSerializer(serializers.ModelSerializer):
+    default_error_messages = {
+        "bad_steps_base": _("This operation type depends on operations with less forecast steps."),
+        "bad_steps_depended": _("This operation type is dependency of operations with bigger forecast steps."),
+    }
+
     operation_type_name = OperationTypeNameSerializer(read_only=True)
     operation_type_name_id = serializers.IntegerField(write_only=True)
     load_template_id = serializers.IntegerField()
@@ -21,6 +31,27 @@ class OperationTypeTemplateSerializer(serializers.ModelSerializer):
                 fields=['load_template_id', 'operation_type_name_id'],
             ),
         ]
+
+    def update(self, instance, validated_data):
+        new_timestep = validated_data.get('forecast_step')
+        impossible_dependences = []
+        impossible_bases = []
+        if new_timestep == timedelta(days=1):
+            impossible_dependences = [timedelta(hours=1), timedelta(minutes=30),]
+        elif new_timestep == timedelta(hours=1):
+            impossible_dependences = [timedelta(minutes=30),]
+            impossible_bases = [timedelta(days=1),]
+        elif new_timestep == timedelta(minutes=30):
+            impossible_bases = [timedelta(days=1),timedelta(hours=1)]
+
+        dependences = instance.depends.filter(forecast_step__in=impossible_dependences).exists()
+        if dependences:
+            raise FieldError(self.error_messages["bad_steps_base"])
+        bases = instance.bases.filter(forecast_step__in=impossible_bases).exists()
+        if bases:
+            raise FieldError(self.error_messages["bad_steps_depended"])
+        return super().update(instance, validated_data)
+
 
 
 class OperationTypeTemplateFilter(FilterSet):
