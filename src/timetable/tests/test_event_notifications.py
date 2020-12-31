c2c3516baf20f03f5178dd007429c3734c9ac240
Anton250 <antosha250900@gmail.com>
2020-12-31 11:09:50 +0300

    workload - fix tests

--- src/timetable/tests/test_event_notifications.py
+++ src/timetable/tests/test_event_notifications.py
@@ -44,20 +44,21 @@ class TestRequestApproveEventNotifications(TestsHelperMixin, APITestCase):
         self.client.force_authenticate(user=self.user_dir)
 
     def test_request_approve_email_notification_sent(self):
-        subject = 'Запрос на подтверждение графика'
-        event_email_notification = EventEmailNotification.objects.create(
-            event_type=self.request_approve_event,
-            shop_ancestors=True,
-            system_email_template='notifications/email/request_approve.html',
-            subject=subject,
-        )
-        event_email_notification.shop_groups.add(self.group_urs)
-        with mock.patch.object(transaction, 'on_commit', lambda t: t()):
-            resp = self.client.post(self.get_url('WorkerDay-request-approve'), data={'shop_id': self.shop.id})
-        self.assertEqual(resp.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(mail.outbox), 1)
-        self.assertEqual(mail.outbox[0].subject, subject)
-        self.assertEqual(mail.outbox[0].to[0], self.user_urs.email)
+        with self.settings(CELERY_TASK_ALWAYS_EAGER=True):
+            subject = 'Запрос на подтверждение графика'
+            event_email_notification = EventEmailNotification.objects.create(
+                event_type=self.request_approve_event,
+                shop_ancestors=True,
+                system_email_template='notifications/email/request_approve.html',
+                subject=subject,
+            )
+            event_email_notification.shop_groups.add(self.group_urs)
+            with mock.patch.object(transaction, 'on_commit', lambda t: t()):
+                resp = self.client.post(self.get_url('WorkerDay-request-approve'), data={'shop_id': self.shop.id})
+            self.assertEqual(resp.status_code, status.HTTP_200_OK)
+            self.assertEqual(len(mail.outbox), 1)
+            self.assertEqual(mail.outbox[0].subject, subject)
+            self.assertEqual(mail.outbox[0].to[0], self.user_urs.email)
 
 
 class TestApproveEventNotifications(TestsHelperMixin, APITestCase):
@@ -106,57 +107,59 @@ class TestApproveEventNotifications(TestsHelperMixin, APITestCase):
         self.client.force_authenticate(user=self.user_urs)
 
     def test_approve_notification_sent_to_director(self):
-        subject = 'График в магазине {{ shop.name }} был подтвержден'
-        event_email_notification = EventEmailNotification.objects.create(
-            event_type=self.approve_event_type,
-            system_email_template='notifications/email/request_approve.html',
-            subject=subject,
-        )
-        event_email_notification.shop_groups.add(self.group_dir)
-        approve_data = {
-            'shop_id': self.shop.id,
-            'dt_from': self.dt,
-            'dt_to': self.dt,
-            'is_fact': False,
-            'wd_types': [
-                WorkerDay.TYPE_WORKDAY,
-            ],
-        }
-        with mock.patch.object(transaction, 'on_commit', lambda t: t()):
-            resp = self.client.post(self.get_url('WorkerDay-approve'), data=approve_data)
-        self.assertEqual(resp.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(mail.outbox), 1)
-        self.assertEqual(mail.outbox[0].subject, 'График в магазине {} был подтвержден'.format(self.shop.name))
-        self.assertEqual(mail.outbox[0].to[0], self.user_dir.email)
+        with self.settings(CELERY_TASK_ALWAYS_EAGER=True):
+            subject = 'График в магазине {{ shop.name }} был подтвержден'
+            event_email_notification = EventEmailNotification.objects.create(
+                event_type=self.approve_event_type,
+                system_email_template='notifications/email/request_approve.html',
+                subject=subject,
+            )
+            event_email_notification.shop_groups.add(self.group_dir)
+            approve_data = {
+                'shop_id': self.shop.id,
+                'dt_from': self.dt,
+                'dt_to': self.dt,
+                'is_fact': False,
+                'wd_types': [
+                    WorkerDay.TYPE_WORKDAY,
+                ],
+            }
+            with mock.patch.object(transaction, 'on_commit', lambda t: t()):
+                resp = self.client.post(self.get_url('WorkerDay-approve'), data=approve_data)
+            self.assertEqual(resp.status_code, status.HTTP_200_OK)
+            self.assertEqual(len(mail.outbox), 1)
+            self.assertEqual(mail.outbox[0].subject, 'График в магазине {} был подтвержден'.format(self.shop.name))
+            self.assertEqual(mail.outbox[0].to[0], self.user_dir.email)
 
     def test_approve_notification_not_sent_to_event_author(self):
-        self.client.force_authenticate(user=self.user_dir)
-        FunctionGroup.objects.create(group=self.group_dir, func='WorkerDay_approve', method='POST')
-        GroupWorkerDayPermission.objects.create(
-            group=self.group_dir,
-            worker_day_permission=WorkerDayPermission.objects.get(
-                action=WorkerDayPermission.APPROVE,
-                graph_type=WorkerDayPermission.PLAN,
-                wd_type=WorkerDay.TYPE_WORKDAY,
-            ),
-        )
-        subject = 'График в магазине {{ shop.name }} был подтвержден'
-        event_email_notification = EventEmailNotification.objects.create(
-            event_type=self.approve_event_type,
-            system_email_template='notifications/email/request_approve.html',
-            subject=subject,
-        )
-        event_email_notification.shop_groups.add(self.group_dir)
-        approve_data = {
-            'shop_id': self.shop.id,
-            'dt_from': self.dt,
-            'dt_to': self.dt,
-            'is_fact': False,
-            'wd_types': [
-                WorkerDay.TYPE_WORKDAY,
-            ],
-        }
-        with mock.patch.object(transaction, 'on_commit', lambda t: t()):
-            resp = self.client.post(self.get_url('WorkerDay-approve'), data=approve_data)
-        self.assertEqual(resp.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(mail.outbox), 0)
+        with self.settings(CELERY_TASK_ALWAYS_EAGER=True):
+            self.client.force_authenticate(user=self.user_dir)
+            FunctionGroup.objects.create(group=self.group_dir, func='WorkerDay_approve', method='POST')
+            GroupWorkerDayPermission.objects.create(
+                group=self.group_dir,
+                worker_day_permission=WorkerDayPermission.objects.get(
+                    action=WorkerDayPermission.APPROVE,
+                    graph_type=WorkerDayPermission.PLAN,
+                    wd_type=WorkerDay.TYPE_WORKDAY,
+                ),
+            )
+            subject = 'График в магазине {{ shop.name }} был подтвержден'
+            event_email_notification = EventEmailNotification.objects.create(
+                event_type=self.approve_event_type,
+                system_email_template='notifications/email/request_approve.html',
+                subject=subject,
+            )
+            event_email_notification.shop_groups.add(self.group_dir)
+            approve_data = {
+                'shop_id': self.shop.id,
+                'dt_from': self.dt,
+                'dt_to': self.dt,
+                'is_fact': False,
+                'wd_types': [
+                    WorkerDay.TYPE_WORKDAY,
+                ],
+            }
+            with mock.patch.object(transaction, 'on_commit', lambda t: t()):
+                resp = self.client.post(self.get_url('WorkerDay-approve'), data=approve_data)
+            self.assertEqual(resp.status_code, status.HTTP_200_OK)
+            self.assertEqual(len(mail.outbox), 0)
