e8e56b94edf459f0c4982472aacf2bba1747977f
Anton250 <antosha250900@gmail.com>
2021-04-06 09:35:27 +0300

    1345 - added notification when employment working not accarding to plan

--- src/notifications/templates.py
+++ src/notifications/templates.py
@@ -4,4 +4,5 @@ SYSTEM_EMAIL_TEMPLATES = [
     ('notifications/email/request_approve.html', 'Подразделение {{ shop.name }} запрашивает подтверждения графика'),
     ('notifications/email/approve.html', 'График в подразделении {{ shop.name }} подтвержден'),
     ('notifications/email/employee_not_checked_in.html', 'Сотрудник {{ user.last_name }} {{ user.first_name }} не отметился на {{ type }} в {{ dttm }}.'),
+    ('notifications/email/employee_working_not_according_to_plan.html', 'Сотрудник {{ user.last_name }} {{ user.first_name }} вышел не по плану в {{ dttm }}.'),
 ]
