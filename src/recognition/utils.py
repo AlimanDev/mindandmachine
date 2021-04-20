3ea36e04ff845c65013620a41cb69c927b999db8
Anton250 <antosha250900@gmail.com>
2021-04-20 10:40:04 +0300

    1361 - changed tabel code getting

--- src/recognition/utils.py
+++ src/recognition/utils.py
@@ -1,6 +1,6 @@
 from src.timetable.models import PlanAndFactHours, WorkerDay
-from datetime import timedelta, datetime
-from src.base.models import Shop, User
+from datetime import timedelta, datetime, date
+from src.base.models import Shop, User, Employment
 from src.recognition.api.recognition import Recognition
 from src.recognition.models import UserConnecter
 from src.recognition.events import DUPLICATE_BIOMETRICS
@@ -64,19 +64,26 @@ def check_duplicate_biometrics(image, user: User):
             return 'User from other system'
         if user.id == user_connecter.user_id:
             return
+        user2 = user_connecter.user
         notifications = EventEmailNotification.objects.filter(
             code=DUPLICATE_BIOMETRICS,
         )
+        active_employments = Employment.objects.get_active(
+            dt_from=date.today(), 
+            dt_to=date.today(),
+        )
+        employment1 = active_employments.filtter(user=user).first() or Employment.objects.filter(user=user).order_by('-dttm_fired').first()
+        employment2 = active_employments.filtter(user=user2).first() or Employment.objects.filter(user=user2).order_by('-dttm_fired').first()
         for notification in notifications:
             send_event_email_notifications.delay(
                 notification.id, 
                 None, 
                 {
                     'fio1': f"{user.last_name} {user.first_name}",
-                    'fio2': f"{user_connecter.user.last_name} {user_connecter.user.last_name}",
+                    'fio2': f"{user2.last_name} {user2.last_name}",
                     'url1': settings.HOST + user.avatar.url,
-                    'url2': settings.HOST + user_connecter.user.avatar.url,
-                    'tabel_code1': user.username,
-                    'tabel_code2': user_connecter.user.username,
+                    'url2': settings.HOST + user2.avatar.url,
+                    'tabel_code1': (employment1.tabel_code if employment1 else user.tabel_code) or user.username,
+                    'tabel_code2': (employment2.tabel_code if employment2 else user2.tabel_code) or user2.username,
                 },
             )
