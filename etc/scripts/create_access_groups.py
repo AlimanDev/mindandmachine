7189a1919bee6d0e7205110c2586b0fee0745435
Dmitriev <dmitriev_as@srvhub.ru>
2021-05-04 13:56:56 +0300

    allow to update groups permissions from file

--- etc/scripts/create_access_groups.py
+++ etc/scripts/create_access_groups.py
@@ -18,7 +18,7 @@ def password_generator(len=14):
     return str(uuid4()).replace('-', '')[:len]  # algo based on SHA-1 (not safe enough nowdays)
 
 
-def create_group_functions(path=None, network=None, verbose=True):
+def update_group_functions(path=None, network=None, verbose=True):
     if path is None:
         path = 'etc/scripts/function_group_default.xlsx'
 
@@ -28,18 +28,27 @@ def create_group_functions(path=None, network=None, verbose=True):
     df_funcs = pd.read_excel(path)
 
     for group_name in df_funcs['group'].unique():
-        group = Group.objects.create(name=group_name, network=network)
-        fgs = FunctionGroup.objects.bulk_create([
-            FunctionGroup(
+        group, _group_created = Group.objects.get_or_create(name=group_name, network=network)
+        fgs_created = []
+        fgs_exists = []
+        for _, func in df_funcs[df_funcs['group'] == group_name].iterrows():
+            fg, _fg_created = FunctionGroup.objects.get_or_create(
                 group=group,
                 func=func['func'],
                 method=func['method'],
-                level_up=1,
-                level_down=100,
-            ) for _, func in df_funcs[df_funcs['group'] == group_name].iterrows()
-        ])
+                defaults=dict(
+                    level_up=1,
+                    level_down=100,
+                    access_type='all',
+                )
+            )
+            if _fg_created:
+                fgs_created.append(fg)
+            else:
+                fgs_exists.append(fg)
         if verbose:
-            print(f'created {group}, with {len(fgs)} access')
+            print(
+                f'group {"created" if _group_created else "exists"} {group.name}; perms exists: {len(fgs_exists)}, created: {len(fgs_created)}')
 
 #
 # def main(hq_accs=2):
