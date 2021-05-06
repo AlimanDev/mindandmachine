4f63e689dd10d63fe6035704743892ef102027ec
Dmitriev <dmitriev_as@srvhub.ru>
2021-05-06 16:37:34 +0300

    1361 - add DuplicateBiometricsEvent in __init__.py

--- src/recognition/__init__.py
+++ src/recognition/__init__.py
@@ -1,2 +1,2 @@
 default_app_config = 'src.recognition.apps.RecognitionConfig'
-from .events import EmployeeNotCheckedInEvent, EmployeeWorkingNotAccordingToPlanEvent
+from .events import EmployeeNotCheckedInEvent, EmployeeWorkingNotAccordingToPlanEvent, DuplicateBiometricsEvent
