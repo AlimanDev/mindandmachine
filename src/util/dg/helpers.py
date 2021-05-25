5e14f61a12c97278d5f0fb3ae8b2667143479197
Anton250 <antosha250900@gmail.com>
2021-05-25 10:54:26 +0300

    1767 - added translation for timetable

--- src/util/dg/helpers.py
+++ src/util/dg/helpers.py
@@ -1,7 +1,7 @@
 import os
 
 from relatorio.templates.opendocument import Template
-
+from django.utils.translation import gettext as _
 
 def generate_document(template_path, data):
     basic = Template(source='', filepath=template_path)
@@ -15,16 +15,16 @@ def get_extension(file_name):
 
 
 MONTH_NAMES = {
-    1: 'Январь',
-    2: 'Февраль',
-    3: 'Март',
-    4: 'Апрель',
-    5: 'Май',
-    6: 'Июнь',
-    7: 'Июль',
-    8: 'Август',
-    9: 'Сентябрь',
-    10: 'Октябрь',
-    11: 'Ноябрь',
-    12: 'Декабрь',
+    1: _('January'),
+    2: _('February'),
+    3: _('March'),
+    4: _('April'),
+    5: _('May'),
+    6: _('June'),
+    7: _('July'),
+    8: _('August'),
+    9: _('September'),
+    10: _('October'),
+    11: _('November'),
+    12: _('December'),
 }
