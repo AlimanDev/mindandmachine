ac28efd7a078321057d2caeaed3a2bef43a9902a
Dmitriev <dmitriev_as@srvhub.ru>
2021-06-28 17:45:37 +0300

    1831 - add XMLRenderer to DEFAULT_RENDERER_CLASSES

--- src/conf/djconfig.py
+++ src/conf/djconfig.py
@@ -95,6 +95,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
+        'rest_framework_xml.renderers.XMLRenderer',
     ],
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
