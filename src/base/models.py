268ec8de814e0e166331c46a15691763483db460
Dmitriev <dmitriev_as@srvhub.ru>
2021-08-04 11:52:12 +0300

    DEV-103 - allow to update workerday with employee from outsourcing network

--- src/base/models.py
+++ src/base/models.py
@@ -725,7 +725,7 @@ class EmploymentManager(models.Manager):
             models.Q(dttm_deleted__date__gt=timezone.now().date()) | models.Q(dttm_deleted__isnull=True)
         )
 
-    def get_active(self, network_id=None, dt_from=None, dt_to=None, *args, **kwargs):
+    def get_active(self, network_id=None, dt_from=None, dt_to=None, extra_q=None, **kwargs):
         """
         hired earlier then dt_from, hired later then dt_to
         :param network_id:
@@ -748,13 +748,14 @@ class EmploymentManager(models.Manager):
                 shop__network_id=network_id,
                 employee__user__network_id=network_id,
             )
-        qs = self.filter(q)
-        return qs.filter(*args, **kwargs)
+        if extra_q:
+            q &= extra_q
+        return self.filter(q, **kwargs)
 
     def get_active_empl_by_priority(
-            self, network_id, dt=None, priority_shop_id=None, priority_employment_id=None,
-            priority_work_type_id=None, priority_by_visible=True, **kwargs):
-        qs = self.get_active(network_id, dt_from=dt, dt_to=dt, **kwargs)
+            self, network_id=None, dt=None, priority_shop_id=None, priority_employment_id=None,
+            priority_work_type_id=None, priority_by_visible=True, extra_q=None, **kwargs):
+        qs = self.get_active(network_id=network_id, dt_from=dt, dt_to=dt, extra_q=extra_q, **kwargs)
 
         order_by = []
 
