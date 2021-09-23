854899d4ee64d98b63130d5ff1be270681d27cfd
Dmitriev <dmitriev_as@srvhub.ru>
2021-09-23 23:30:06 +0300

    DEV-213 - read file from ftp

--- src/exchange/fs_engines/ftp.py
+++ src/exchange/fs_engines/ftp.py
@@ -1,10 +1,24 @@
+from ftplib import FTP
+from io import BytesIO
+
 from .base import FilesystemEngine
 
 
 class FtpEngine(FilesystemEngine):
     def __init__(self, host, port, username, password, **kwargs):
         self.host = host
-        self.port = port
+        self.port = port  # пока лишний получется...
         self.username = username
         self.password = password
         super(FtpEngine, self).__init__(**kwargs)
+        self.ftp = self._init_ftp()
+
+    def _init_ftp(self):
+        ftp = FTP(host=self.host, user=self.username, passwd=self.password)
+        ftp.cwd(self.base_path)
+        return ftp
+
+    def read_file(self, filename):
+        r = BytesIO()
+        self.ftp.retrbinary(f'RETR {filename}', r.write)
+        return r.getvalue()
