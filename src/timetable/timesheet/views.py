a5043e9de2ed54c3c30420ccafa987ed07fd963f
Dmitriev <dmitriev_as@srvhub.ru>
2021-10-05 16:29:45 +0300

    recalc timesheet fast fix

--- src/timetable/timesheet/views.py
+++ src/timetable/timesheet/views.py
@@ -81,5 +81,9 @@ class TimesheetViewSet(BaseModelViewSet):
         employee_ids = list(employee_ids)
         if not employee_ids:
             raise ValidationError({'detail': _('No employees satisfying the conditions.')})
-        calc_timesheets.delay(employee_id__in=list(employee_ids))
+        calc_timesheets.delay(
+            employee_id__in=list(employee_ids),
+            dt_from=serializer.validated_data['dt_from'],
+            dt_to=serializer.validated_data['dt_to'],
+        )
         return Response({'detail': _('Timesheet recalculation started successfully.')})
