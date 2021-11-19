450386f245fb0b5b8f36bae354434169f79a63ef
Anton250 <antonp0925@gmail.com>
2021-11-19 11:30:33 +0300

    dev-433 - receipt store optimization

--- src/base/fields.py
+++ src/base/fields.py
@@ -1,6 +1,8 @@
+import zlib
+import base64
+
 from django.utils import timezone
-from django.db.models import Q
-from src.base.models import Employment
+from django.db.models import Q, TextField
 
 
 class CurrentUserNetwork:
@@ -15,6 +17,7 @@ class UserworkShop:
     requires_context = True
 
     def __call__(self, serializer_field):
+        from src.base.models import Employment
         now_day = timezone.now().date()
         employment =  Employment.objects.filter(
             Q(dt_fired__gte=now_day) | Q(dt_fired__isnull=True),
@@ -22,3 +25,24 @@ class UserworkShop:
         ).first()
         return employment.shop_id if employment else None
 
+
+class CompressedTextField(TextField):
+    def to_python(self, value):
+        try:
+            return zlib.decompress(base64.b64decode(bytes(value, 'utf-8'))).decode('utf-8')
+        except:
+            return value
+
+    def value_from_object(self, obj):
+        return self.to_python(getattr(obj, self.attname))
+
+    def get_db_prep_save(self, value, connection):
+        value = self.get_db_prep_value(value, connection=connection, prepared=False)
+        return base64.b64encode(zlib.compress(bytes(value, 'utf-8'))).decode('utf-8')
+
+    def get_prep_value(self, value):
+        value = super().get_prep_value(value)
+        return self.to_python(value)
+
+    def from_db_value(self, value, expression, connection):
+        return self.to_python(value)
