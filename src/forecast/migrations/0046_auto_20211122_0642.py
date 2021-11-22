bda9124eaf56bf8113e59bbf503f1724bb1fc0d0
Anton250 <antonp0925@gmail.com>
2021-11-22 10:13:01 +0300

    dev-433 - added ordering

--- src/forecast/migrations/0046_auto_20211122_0642.py
+++ src/forecast/migrations/0046_auto_20211122_0642.py
@@ -4,7 +4,7 @@ from django.db import migrations
 
 def compress_info(apps, schema_editor):
     Receipt = apps.get_model('forecast', 'Receipt')
-    receipts = Receipt.objects.all()
+    receipts = Receipt.objects.all().order_by('id')
     step = 100000
     receipts_portion = receipts[:step]
     i = step
