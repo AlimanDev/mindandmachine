478108c248e0a665b685d147b8698db8ba0d5d61
Dmitriev <dmitriev_as@srvhub.ru>
2021-12-16 20:58:14 +0300

    DEV-571 - add day and night hours in shift schedule, improve divider

--- src/base/shift_schedule/tests.py
+++ src/base/shift_schedule/tests.py
@@ -268,12 +268,16 @@ class TestShiftScheduleIntervalViewSet(TestsHelperMixin, APITestCase):
                         "dt": "2021-01-01",
                         "day_type": "W",
                         "work_hours": "11",
+                        "day_hours": "11",
+                        "night_hours": "0",
                     },
                     {
                         "code": "1_2021-01-07",
                         "dt": "2021-01-07",
                         "day_type": "W",
                         "work_hours": "13",
+                        "day_hours": "13",
+                        "night_hours": "0",
                     },
                 ]
             },
@@ -285,13 +289,17 @@ class TestShiftScheduleIntervalViewSet(TestsHelperMixin, APITestCase):
                         "code": "2_2021-01-01",
                         "dt": "2021-01-01",
                         "day_type": "W",
-                        "work_hours": "13",
+                        "work_hours": "11",
+                        "day_hours": "11",
+                        "night_hours": "0",
                     },
                     {
                         "code": "2_2021-01-07",
                         "dt": "2021-01-07",
                         "day_type": "W",
                         "work_hours": "13",
+                        "day_hours": "13",
+                        "night_hours": "0",
                     },
                 ]
             },
@@ -304,12 +312,16 @@ class TestShiftScheduleIntervalViewSet(TestsHelperMixin, APITestCase):
                         "dt": "2022-01-01",
                         "day_type": "W",
                         "work_hours": "16",
+                        "day_hours": "16",
+                        "night_hours": "0",
                     },
                     {
                         "code": "3_2022-01-07",
                         "dt": "2022-01-07",
                         "day_type": "W",
                         "work_hours": "11",
+                        "day_hours": "11",
+                        "night_hours": "0",
                     },
                 ]
             }
