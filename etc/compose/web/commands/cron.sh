718450918bb16762971818e61cd5a1740fe57f47
Anton Petrov <anton@Antons-MacBook-Pro.local>
2022-02-22 10:48:49 +0300

    rnd-86 - added config generator

--- etc/compose/web/commands/cron.sh
+++ etc/compose/web/commands/cron.sh
@@ -3,6 +3,7 @@
 set -e
 
 touch /webapp/logs/cron.log
+python3 /generate_configs.py
 echo "" >> /cron.txt
 crontab /cron.txt
 cron && tail -f /webapp/logs/cron.log
---
+++ etc/compose/web/logrotate/generate_configs.py
@@ -0,0 +1,40 @@
+import os
+
+LOG_DIRS = {
+    '/webapp/logs/uwsgi/': 
+'''
+ {
+    size 10M
+    rotate 100000
+    maxage 90
+    missingok
+    compress
+    delaycompress
+    copytruncate
+    notifempty
+    create 0640 root root
+}
+''',
+    '/webapp/logs/':
+'''
+ {
+    size 100M
+    rotate 100000
+    maxage 90
+    missingok
+    compress
+    delaycompress
+    notifempty
+    create 0640 root root
+}
+''',
+}
+
+for dir, config in LOG_DIRS.items():
+    log_files = os.listdir(dir)
+    for log_file in log_files:
+        config_path = os.path.join('/etc/logrotate.d/', log_file.replace('.log', ''))
+        if not os.path.exists(config_path):
+            with open(config_path, 'r') as f:
+                f.write(os.path.join(dir, log_file) + config)
+
