c3f5d2a8aa9de341b476da0a56696526abe58dc8
Anton Petrov <anton@Antons-MacBook-Pro.local>
2022-03-01 14:44:22 +0300

    rnd-139 - added validation for employment work type

--- src/timetable/serializers.py
+++ src/timetable/serializers.py
@@ -1,3 +1,4 @@
+from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 
@@ -27,3 +28,17 @@ class EmploymentWorkTypeSerializer(serializers.ModelSerializer):
                 fields=['work_type_id', 'employment_id'],
             ),
         ]
+
+    def validate(self, attrs):
+        attrs = super().validate(attrs)
+        check_priority_qs = EmploymentWorkType.objects.filter(
+            employment_id=attrs['employment_id'],
+            priority=attrs.get('priority', 1),
+        )
+        if self.instance:
+            check_priority_qs = check_priority_qs.exclude(id=self.instance.id)
+        
+        if 'priority' in attrs and check_priority_qs.exists():
+            raise serializers.ValidationError(_('Employment can have only one main type of work.'))
+
+        return attrs
