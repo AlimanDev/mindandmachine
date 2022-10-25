0bd52314cbafa150e5ea8fb87dc82f7e7881e510
Podkosa <artem_podkolzin@inbox.ru>
2022-10-25 13:32:40 +0300

    RND-515 tweak message text

--- src/util/email/messages.py
+++ src/util/email/messages.py
@@ -2,5 +2,12 @@ from django.utils.translation import gettext as _
 
 
 def prepare_message_tick_report(url: str):
-    message = _('Your report is available at the link:')
+    message = _("""\
+Your employee monitoring report is ready to be downloaded.
+You can access it by the following link:
+
+{}
+
+Warning: the link will expire at the end of the business day. Please use employee monitoring generation form in the application to create a new version."""
+    ).format(url)
     return message + '\n' + url
