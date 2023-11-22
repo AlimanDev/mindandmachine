0a21b435e6128f15e6291560054f0c098a974fc2
AlimanDev <alimandeveloper@gmail.com>
2023-11-22 13:50:12 +0500

    временно убран eventlet, чтобы анализировать нагрузку без использования потоков

--- etc/compose/web/commands/celery_main.sh
+++ etc/compose/web/commands/celery_main.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-celery -A src.adapters.celery worker -n main -P eventlet --max-tasks-per-child=100 --max-memory-per-child=25000 --loglevel=INFO --logfile=/webapp/logs/celery_main.log
+celery -A src.adapters.celery worker -n main --max-tasks-per-child=100 --max-memory-per-child=25000 --loglevel=INFO --logfile=/webapp/logs/celery_main.log
